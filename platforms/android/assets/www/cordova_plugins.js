6d8104755eb2369439c8b2f3ae0b0accdb7e8a86
Felipe <felipeventurino@hotmail.com>
2014-05-18 22:05:45 -0300

    primeiro commit para bitbucket

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -76,15 +76,31 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/network.js",
+        "id": "org.apache.cordova.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
+        "id": "org.apache.cordova.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.battery-status": "0.2.9-dev",
-    "org.apache.cordova.contacts": "0.2.11-dev",
+    "org.apache.cordova.contacts": "0.2.10",
     "org.apache.cordova.device": "0.2.9",
-    "org.apache.cordova.console": "0.2.8"
+    "org.apache.cordova.console": "0.2.8",
+    "org.apache.cordova.network-information": "0.2.8"
 }
 // BOTTOM OF METADATA
 });
